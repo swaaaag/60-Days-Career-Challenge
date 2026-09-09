c62e7d7c5d379f2b1603baa26948a9c4b317373d
Swagath <swagathsuresh007@gmail.com>
2026-09-10 01:16:05 +0530

    Day01 python2

---
+++ Python/simple_calculator.py
@@ -0,0 +1,12 @@
+print("simple Calculator")
+
+num1=int(input("Enter the number1: \n"))
+num2=int(input("Enter the number2: \n"))
+sum=num1+num2
+mult=num1*num2
+div=num1/num2
+sub=num1-num2
+print("sum of ",num1,"and ", num2,"is ",sum)
+print("multiplication of ",num1,"and ", num2,"is ",mult)
+print("Division of ",num1,"and ", num2,"is ",div)
+print("Substraction of ",num1,"and ", num2,"is ",sub)
